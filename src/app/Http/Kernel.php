0f7ecfa69323689ac8f9f4f8adaf70e2bced3ea9
Eri Suzuki <sekka.alice@gmail.com>
2023-10-26 23:13:53 +0900

    [add] 自作Middlewareを用いたchatできる相手の制限。
    ・日本人は外国人と会話できるが、日本人とは会話できない。

--- src/app/Http/Kernel.php
+++ src/app/Http/Kernel.php
@@ -65,5 +65,6 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'chat' => \App\Http\Middleware\ChatPermission::class, // chat用のmiddleware
     ];
 }
